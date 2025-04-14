a0902c21d7c4956d3d80bef5902b7129b540f730
Patrimonio2-web <patrimoniolegislaturalarioja@gmail.com>
2025-04-14 09:10:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 CORS(app)
 
 # Configuración de la base de datos PostgreSQL
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://patrimonio_ppfk_user:SabopRq1mqHqRXBZaZBaWsEcqfHYJWM2@dpg-cv8oiprqf0us73bbbbfg-a.oregon-postgres.render.com/patrimonio_ppfk"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://patrimonio2_user:27YkVyygLRvqOXZYzlq9zWEEyuet6NLS@dpg-cvuflmhr0fns73829kl0-a.oregon-postgres.render.com/patrimonio2"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # Limite de 16MB para archivos
 
